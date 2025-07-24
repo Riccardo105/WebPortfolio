f27dc10b3ffac1e70e04e12ff88052080e2fd824
Riccardo Barone <riccardo.ComputerScience@gmail.com>
2025-07-24 23:49:29 +0100

    #24 minor change for vecel deployment trigger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "components/header";
 import Footer from "components/footer";
-import ServiceWorkerRegister from "../components/serviceWorkerRegister";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
